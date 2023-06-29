48e3307a68408e4c35e59a1f9512e34b3d49250c
Gareth Wright <garpunkal@gmail.com>
2023-06-29 12:13:11 +0100

    more updates

--- punkOptimise/App_Plugins/punkOptimise/backoffice/punkOptimise.resources.js
+++ punkOptimise/App_Plugins/punkOptimise/backoffice/punkOptimise.resources.js
@@ -1,12 +1,7 @@
 ﻿ angular.module("umbraco.resources")
     .factory("punkOptimiseResources", function ($http) {
         return {
-            save: function (model) {
-                return $http.post('backoffice/punkOptimise/punkOptimise/save', model);
-            },
-
-            isValid: function (id) {
-                return $http.get('backoffice/punkOptimise/punkOptimise/isvalid?id=' + id);
-            }
+            save: function (model) { return $http.post('backoffice/punkOptimise/punkOptimise/save', model); },
+            isValid: function (id) { return $http.get('backoffice/punkOptimise/punkOptimise/isvalid?id=' + id); }
         };
     });   
